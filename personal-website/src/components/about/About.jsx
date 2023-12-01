948b5963e07432a41f3ab410068f92883e306eaf
jurbster <jurbster15@yahoo.com>
2023-12-01 09:57:15 -0600

    small things, changed pic, added lines, etc.

--- personal-website/src/components/about/About.jsx
+++ personal-website/src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-image6.jpg'
+import ME from '../../assets/wedding2.jpg'
 import {FaAward} from 'react-icons/fa'
 
 const About = () => {
@@ -39,7 +39,7 @@ const About = () => {
           </div>
 
           <p>
-            I graduated from Middle Tennessee State University with my Bachelors in Computer Science. I am newly married (October 2023), but I have been with my husband for almost ten years. I have a black cat named Sucy, and an Australian Shepherd named Paxton. I love to go hiking, especially with anywhere that has a water feature. I also love watching TV/movies, and playing video/board games!
+            I graduated from Middle Tennessee State University with my Bachelors in Computer Science. I am newly married (October 2023), but I have been with my husband for almost ten years. I have a black cat named Sucy, and an Australian Shepherd named Paxton. I love to go hiking, especially anywhere that has a water feature. I also love watching TV/movies, and playing video/board games!
           </p>
 
           <a href="#contact" className='btn btn-primary'>Let's Chat</a>
